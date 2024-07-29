45353d5079fd3e2a25095938d14adf0c4aaa8080
jacobbien <jbien@usc.edu>
2024-07-29 14:18:05 -0700

    Distinguish between current/past authors

--- jsm2024/R/authors.R
+++ jsm2024/R/authors.R
@@ -1,6 +1,6 @@
 # Generated from create-jsm2024.Rmd: do not edit by hand
 
 #' JSM author names
-#' @format A character vector of 12,628 JSM author names
+#' @format A character vector of 12,628 names from past and present JSMs
 #' @source This data was from the JSM 2022-2024 programs.
 "authors"
---
+++ jsm2024/R/authors2024.R
@@ -0,0 +1,6 @@
+# Generated from create-jsm2024.Rmd: do not edit by hand
+
+#' JSM 2024 author names
+#' @format A character vector of 5,820 JSM author names
+#' @source This data was from the JSM 2024 program.
+"authors2024"
